d6ac7f10dcf65280c88eb00017bfbe0a8e318e1c
Waliston Belles <walistonbelles1@gmail.com>
2021-12-31 12:11:15 -0300

    build: adicionado dependência do lucid

--- backend/env.ts
+++ backend/env.ts
@@ -1,17 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Validating Environment Variables
-|--------------------------------------------------------------------------
-|
-| In this file we define the rules for validating environment variables.
-| By performing validation we ensure that your application is running in
-| a stable environment with correct configuration values.
-|
-| This file is read automatically by the framework during the boot lifecycle
-| and hence do not rename or move this file to a different location.
-|
-*/
-
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
@@ -21,4 +7,12 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  
+  DB_CONNECTION: Env.schema.string(),
+
+  MYSQL_HOST: Env.schema.string({ format: 'host' }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string.optional(),
+  MYSQL_DB_NAME: Env.schema.string(),
 })
