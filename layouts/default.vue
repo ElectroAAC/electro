086107df3b1263da14d9aff504ddfbe447050a97
Waliston Belles <walistonbelles1@gmail.com>
2021-12-30 23:51:02 -0300

    build: configurações para a estrutura base do projeto

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app id="app">
+    <Nuxt/>
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'electro-aac',
+    title: 'Electro - AAC',
     htmlAttrs: {
       lang: 'en',
     },
@@ -11,11 +11,13 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -23,10 +25,15 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  device: {
+    refreshOnResize: true
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    // https://vuetifyjs.com/en/getting-started/installation/#nuxt-install
     '@nuxtjs/vuetify'
   ],
 
@@ -44,4 +51,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  ssr: true
 }
