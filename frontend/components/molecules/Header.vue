b7758218b3540619ffc2c02241c00d7e22cc1d5e
Waliston Belles <walistonbelles1@gmail.com>
2022-01-01 21:19:13 -0300

    refactor: melhorado responsividade do menu superior

--- frontend/components/molecules/Header.vue
+++ frontend/components/molecules/Header.vue
@@ -3,10 +3,7 @@
     app
     id="header"
   >
-    <v-row>
-      <v-col cols="1"> </v-col>
-      
-      <v-col cols="4" class="d-flex align-center justify-center header-text"> 
+    <v-row class="d-flex align-center justify-center header-text">
         <NuxtLink to="/" class="pa-5"> HOME </NuxtLink>
         <v-menu
           open-on-hover
@@ -35,21 +32,12 @@
           </v-list>
         </v-menu>
         <NuxtLink to="/" class="pa-5"> HIGHSCORES </NuxtLink>
-      </v-col>
-
-      <v-col cols="2" class="pt-8 d-flex justify-center"> 
         <NuxtLink to="/">
-          <Logo width="130px"/>
+          <Logo class="mt-8" width="130px"/>
         </NuxtLink>
-      </v-col>
-
-      <v-col cols="4" class="d-flex align-center justify-center header-text"> 
         <NuxtLink to="/" class="pa-5"> COMMUNITY </NuxtLink>
         <NuxtLink to="/" class="pa-5"> SHOP </NuxtLink>
         <NuxtLink to="/" class="pa-5"> DOWNLOAD </NuxtLink>
-      </v-col>
-
-      <v-col cols="1"> </v-col>
     </v-row>
   </v-app-bar>
 </template>
--- frontend/components/molecules/TrailerWidget.vue
+++ frontend/components/molecules/TrailerWidget.vue
@@ -1,16 +1,15 @@
 <template>
   <aside class="widget">
     <h5 class="widget-title"> Trailer </h5>
-    <v-container>
-      <iframe 
-        width="100%"
-        height="315" 
-        src="https://www.youtube.com/embed/G7QZR2OWLR0" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen
-      ></iframe>
-    </v-container>
+    <iframe 
+      class="pt-2"
+      width="100%"
+      height="200" 
+      src="https://www.youtube.com/embed/Dn-nL-wNxX8" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen>
+    </iframe>
   </aside>
 </template>
