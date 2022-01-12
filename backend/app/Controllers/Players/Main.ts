3507e4fc209dd364f4d27069146ef50beaddcd1f
Waliston Belles <walistonbelles1@gmail.com>
2022-01-12 19:32:31 -0300

    [BACK] feat: corrigido endpoint para aceitar nome no lugar do id do personagem

--- backend/app/Controllers/Players/Main.ts
+++ backend/app/Controllers/Players/Main.ts
@@ -4,10 +4,21 @@ import Database from '@ioc:Adonis/Lucid/Database'
 export default class PlayersController {
   public async show({ request, response }: HttpContextContract) {
     try {
+      let name = request.param('name');
+      name = name
+        .replace("%20", " ")
+        .replace("+", " ")
+        .replace("'", "")
+        .replace("%27", "")
+        .replace("-", "")
+        .replace("/", "")
+        .replace('"', "")
+        .replace('%22', "");
+
       const player = await Database
         .from('players')
         .select('name', 'sex', 'vocation', 'level', 'maglevel', 'lastlogin', 'healthmax', 'manamax', 'online', 'created')
-        .where('id', request.param('id'));
+        .where('name', name);
       return response.status(200).send({ result: player});
 
     } catch(err) {
--- backend/start/routes/players.ts
+++ backend/start/routes/players.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route';
 
-Route.get('/player/:id', 'Players/Main.show');
+Route.get('/player/:name', 'Players/Main.show');
 Route.get('/player/:id/skills', 'Players/Skills.show');
 Route.get('/player/:id/storage/:key', 'Players/Storage.show');
 Route.get('/player/:id/deaths', 'Players/Deaths.show');
