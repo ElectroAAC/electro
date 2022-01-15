e8d1f335ab18b33e05625b1a1fabc2bc82188678
Waliston Belles <walistonbelles1@gmail.com>
2022-01-15 16:12:58 -0300

    [BACK] feat: adicionado autenticador nos endpoints de dashboard

--- backend/app/Controllers/Dashboard/Accounts.ts
+++ backend/app/Controllers/Dashboard/Accounts.ts
@@ -2,13 +2,15 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class AccountsController {
-  public async show({ response }: HttpContextContract) {
+  public async show({ response, bouncer }: HttpContextContract) {
     try {
+      await bouncer.with('DashboardPolicy').authorize('viewList');
+      
       const accounts = await Database
-      .from('accounts')
-      .count('* as total')
-      return response.status(200).send({ result: accounts});
+        .from('accounts')
+        .count('* as total');
 
+      return response.status(200).send({ result: accounts});
     } catch(err) {
       console.log('Error getTotalAccounts Query: ', err);
       return response.status(400).send({ message: 'An error occurred, check the api console.'})
--- backend/app/Controllers/Dashboard/Players.ts
+++ backend/app/Controllers/Dashboard/Players.ts
@@ -2,13 +2,15 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class PlayersController {
-  public async show({ response }: HttpContextContract) {
+  public async show({ response, bouncer }: HttpContextContract) {
     try {
-      const accounts = await Database
-      .from('players')
-      .count('* as total')
-      return response.status(200).send({ result: accounts});
+      await bouncer.with('DashboardPolicy').authorize('viewList');
 
+      const accounts = await Database
+        .from('players')
+        .count('* as total');
+
+      return response.status(200).send({ result: accounts});
     } catch(err) {
       console.log('Error getTotalPlayers Query: ', err);
       return response.status(400).send({ message: 'An error occurred, check the api console.'})
