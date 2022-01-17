8ac0db098f4ee952dc643cbf6b0eeb3e8fb841fd
Waliston Belles <walistonbelles1@gmail.com>
2022-01-17 20:42:40 -0300

    [BACK] feat: adicionado endpoint para ranking de experiencia

--- backend/app/Controllers/Highscores/Main.ts
+++ backend/app/Controllers/Highscores/Main.ts
@@ -2,7 +2,6 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class HighscoresController {
-
   public async show({ response }: HttpContextContract) {
     try {
       const topRank = await Database
@@ -14,6 +13,21 @@ export default class HighscoresController {
     } catch(err) {
       console.log('Error getTop5Players Query: ', err);
       return response.status(400).send({ error: 'An error occurred, check the api console.'});
+    }
+  }
+
+  public async getRank({ request, response }: HttpContextContract) {
+    try {
+      const rank = await Database
+        .from('players')
+        .select('name', 'vocation', 'level', 'experience')
+        .orderBy('experience', 'desc')
+        .paginate(request.param('page', 1), request.param('limit'));
+      
+      return response.status(200).send({ result: rank});
+    } catch (err) {
+      console.log('Error getRanking Query: ', err);
+      return response.status(404).send({ error: 'An error ocurred, check the api console.' });
     }
   }
 }
