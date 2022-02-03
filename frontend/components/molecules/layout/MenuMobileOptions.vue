c63a71f2bba8f732dd5037a8c713842381e6a73c
Waliston Belles <walistonbelles1@gmail.com>
2022-02-02 23:35:17 -0300

    [FRONT] feat: mobile menu became drawer

---
+++ frontend/components/molecules/layout/MenuMobileOptions.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-list :class="miniVariant ? '' : 'ml-4 mr-4'">
+    <v-list-item router exact>
+      <v-list-item-content>
+        <v-list-item>
+          <v-list-item-title class="dashboard-menu-text"> <NuxtLink to="/" class="pa-5"> HOME </NuxtLink> </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-treeview
+            v-model="tree"
+            :items="getRoutesCommunity"
+            open-on-click
+            expand-icon="mdi-chevron-down"
+            item-key="text"
+          > 
+            <template v-slot:append="{ item }">
+              <NuxtLink v-if="item.to" class="dashboard-menu-text" :to="item.to"> {{ item.text }} </NuxtLink>
+              <span v-else class="dashboard-menu-text"> {{ item.text }} </span>
+            </template>
+          </v-treeview>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title class="dashboard-menu-text"> <NuxtLink to="/highscores" class="pa-5"> HIGHSCORES </NuxtLink> </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title class="dashboard-menu-text"> <NuxtLink to="/guilds" class="pa-5"> GUILDS </NuxtLink> </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title class="dashboard-menu-text"> <NuxtLink to="/" class="pa-5"> SHOP </NuxtLink> </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title class="dashboard-menu-text"> <NuxtLink to="/downloads" class="pa-5"> DOWNLOAD </NuxtLink> </v-list-item-title>
+        </v-list-item>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { routesCommunity } from '@/utils/routes'
+
+export default Vue.extend({
+  props: {
+    miniVariant: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      tree: []
+    }
+  },
+
+  computed: {
+    getRoutesCommunity() {
+      return [{ text: "Community", children: routesCommunity }];
+    }
+  }
+})
+</script>
---
+++ frontend/components/organisms/layout/MenuMobile.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-navigation-drawer
+    v-model="$_drawer"
+    :mini-variant="miniVariant"
+    :clipped="false"
+    absolute
+    app
+    class="text-center"
+  >      
+    <MenuMobileOptions :miniVariant="miniVariant"/>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    drawer: {
+      type: Boolean,
+      default: false
+    },
+    miniVariant: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    $_drawer: {
+      get(this: any): boolean {
+        return this.drawer;
+      },
+      set(val): void {
+        this.$emit('update-drawer', val);
+      }
+    },
+
+    getItems(): { to: string, title: string, icon: string }[] {
+      return [{
+        to: "",
+        title: "",
+        icon: ""
+      }];
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.theme--light.v-list {
+  background: inherit !important;
+}
+</style>
