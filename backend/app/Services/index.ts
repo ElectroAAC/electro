d8f4f156a76d8031739aef61f33ce5d8f3436bfa
Waliston Belles <walistonbelles1@gmail.com>
2022-02-04 20:19:19 -0300

    [BACK] refactor: standardized export of services

--- backend/app/Services/index.ts
+++ backend/app/Services/index.ts
@@ -1,3 +1,4 @@
 export { default as AccountService } from './AccountService'
 export { default as CharacterService } from './CharacterService'
+export { default as HighscoresService } from './HighscoresService'
 export { default as HouseService } from './HouseService'
