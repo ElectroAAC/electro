47429ee817d7019fee75921eb1b6eef795a4ba15
Waliston Belles <walistonbelles1@gmail.com>
2022-02-04 20:54:18 -0300

    [BACK] feat: created service for online

--- backend/app/Controllers/Online/Main.ts
+++ backend/app/Controllers/Online/Main.ts
@@ -1,17 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
+import { OnlineService } from 'App/Services'
 
 export default class OnlineController {
+  public onlineService: OnlineService = new OnlineService();
+
   public async show({ response }: HttpContextContract) {
     try {
-      const playersOnline = await Database
-        .from('players')
-        .innerJoin('accounts', 'accounts.id', 'players.account_id')
-        .select('accounts.country', 'players.skull', 'players.skulltime', 'players.name', 'players.level', 'players.vocation')
-        .where('players.online', '>', '0')
-        .orderBy('experience', 'desc');
+      const playersOnline = await this.onlineService.getPlayersOnline();
       
-      const serverRecord = await Database.from('server_record').select('record', 'timestamp').where('world_id', '=', 0).orderBy('record', 'desc').limit(1)
+      const serverRecord = await this.onlineService.getRecord(0);
       
       const result = {
         serverRecord,
