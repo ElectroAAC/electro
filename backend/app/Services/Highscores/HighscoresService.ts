cf9743f0b9f1952284dce916b79a1057e55faa2c
Waliston Belles <walistonbelles1@gmail.com>
2022-02-03 22:11:02 -0300

    [BACK] feat: added top kills filter

--- backend/app/Services/Highscores/HighscoresService.ts
+++ backend/app/Services/Highscores/HighscoresService.ts
@@ -13,7 +13,6 @@ export default class HighscoresService {
   }
   
   public async find(page: number, limit: number, type: string): Promise<Object[]> {  
-    console.log(type);
     if (type === 'Experience')  
       return await Database
         .from('players')
@@ -32,13 +31,25 @@ export default class HighscoresService {
         .orderBy('maglevel', 'desc')
         .paginate(page, limit);
 
+    else if (type === 'Frags')
+      return await Database
+        .from('players')
+        .innerJoin('player_killers', 'player_killers.player_id', 'players.id')
+        .select('players.name', 'players.vocation', 'players.online')
+        .count('player_killers.player_id as value')
+        .where('players.deleted', '=', 0)
+        .andWhere('players.group_id', '<', 4)
+        .groupBy('players.name')
+        .orderBy('value', 'desc')
+        .paginate(page, limit);
+
     else {
       const skill = Skills.find((skill) => skill.text === type)?.value;
       
       return await Database
         .from('players')
         .innerJoin('player_skills', 'player_skills.player_id', 'players.id')
-        .select('players.name', 'players.vocation', 'player_skills.value')
+        .select('players.name', 'players.vocation', 'player_skills.value', 'players.online')
         .where('players.deleted', '=', 0)
         .andWhere('players.group_id', '<', 4)
         .andWhere('player_skills.skillid', '=', skill || 0)
