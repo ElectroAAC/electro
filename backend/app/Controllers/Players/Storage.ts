d07f0f19fd65e6c274e8ab43bebb247d92dc3d51
Waliston Belles <walistonbelles1@gmail.com>
2022-02-04 20:49:33 -0300

    [BACK] feat: created service for storage

--- backend/app/Controllers/Players/Storage.ts
+++ backend/app/Controllers/Players/Storage.ts
@@ -1,17 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
+import { StorageService } from 'App/Services'
 
 export default class PlayerStorageController {
+  public storageService: StorageService = new StorageService();
+
   public async show({ request, response }: HttpContextContract) {
     try {
-      const playerStorage = await Database
-        .from('player_storage')
-        .select('player_storage.key', 'player_storage.value')
-        .where('player_storage.player_id', request.param('id'))
-        .andWhere('player_storage.key', request.param('key'));
+      const playerStorage = await this.storageService.getCharacterStorage(request.param('id'), request.param('key'));
 
       return response.status(200).send({ result: playerStorage});
-
     } catch(err) {
       console.log('Error getPlayerStorage Query: ', err);
       return response.status(400).send({ message: 'An error occurred, check the api console.'})
--- backend/app/Services/index.ts
+++ backend/app/Services/index.ts
@@ -1,4 +1,8 @@
 export { default as AccountService } from './AccountService'
 export { default as CharacterService } from './CharacterService'
+export { default as DeathService } from './DeathService'
 export { default as HighscoresService } from './HighscoresService'
+export { default as ItemService } from './ItemService'
 export { default as HouseService } from './HouseService'
+export { default as SkillService } from './SkillService'
+export { default as StorageService } from './StorageService'
