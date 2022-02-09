90ac6447195637a839032f6d07faf6fa900a3678
Waliston Belles <walistonbelles1@gmail.com>
2022-02-08 23:15:27 -0300

    [FRONT] feat: added animation on page 404

--- frontend/components/atoms/Error404.vue
+++ frontend/components/atoms/Error404.vue
@@ -1,3 +1,25 @@
 <template>
-  <img src="~/assets/images/404.png">
+  <lottie-animation path="./404-error.json" />
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue";
+
+export default Vue.extend({
+  components: {
+    LottieAnimation
+  },
+
+  data() {
+    return {
+      options: [
+        {
+          name: "error",
+          path: "./404-error.json",
+        },
+      ],
+    };
+  },
+})
+</script>
