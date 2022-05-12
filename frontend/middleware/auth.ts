90a46f56c36a65f4e1ca3c81ce5a8e6769bebff7
Waliston Belles <walistonbelles1@gmail.com>
2022-05-12 19:54:59 -0300

    [FRONT] feat: added alert message when trying to access a route without authentication

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,7 +1,15 @@
 import { Middleware } from '@nuxt/types'
 
-const auth: Middleware = ({ store, redirect}) => {
+const auth: Middleware = ({ store, redirect, $toast}) => {
   if (!store.state.auth.token) {
+    $toast.error(
+      'You need to be authenticated.',
+      {
+        keepOnHover: true,
+        duration: 2000,
+        theme: "bubble",
+      }
+    )
     return redirect('/');
   }
 }
--- frontend/pages/guilds/create.vue
+++ frontend/pages/guilds/create.vue
@@ -1,3 +1,10 @@
 <template>
   <CreateGuildTemplate />
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  middleware: 'auth',
+})
+</script>
