a33efc5204d106faf7eadbb9dfafced6cf399a4f
Waliston Belles <walistonbelles1@gmail.com>
2022-05-28 10:31:32 -0300

    [BACK] feat: added player outfit in ranking and online endpoints

--- backend/app/Services/OnlineService.ts
+++ backend/app/Services/OnlineService.ts
@@ -3,17 +3,11 @@ import Database from '@ioc:Adonis/Lucid/Database'
 export default class OnlineService {
   public async getPlayersOnline(): Promise<Object[]> {  
     try {
-      /**
-       * 'SELECT `accounts`.`country`, `players`.`name`, `level`, `vocation`' . $outfit . ', `' . $skull_time . '` as `skulltime`, `' . $skull_type . '` as `skull` 
-       * FROM `accounts`, `players`, `players_online` 
-       * WHERE `players`.`id` = `players_online`.`player_id` AND `accounts`.`id` = `players`.`account_id`  
-       * ORDER BY ' . $order)
-       */
       return await Database
         .from('players_online as po')
         .innerJoin('players as p', 'p.id', 'po.player_id')
         .innerJoin('accounts as a', 'a.id', 'p.account_id')
-        .select('a.country', 'p.name', 'p.level', 'p.vocation')
+        .select('a.country', 'p.name', 'p.level', 'p.vocation', 'p.lookhead', 'p.lookbody', 'p.looklegs', 'p.lookfeet', 'p.looktype', 'p.lookaddons')
         .orderBy('p.experience', 'desc');
     } catch (err) {
       console.log(err);
