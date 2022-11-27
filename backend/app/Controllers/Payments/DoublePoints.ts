80c611337f875f6969da9728549dc0b3d833089b
“WalistonBelles” <walistonbelles1@gmail.com>
2022-11-26 21:10:41 -0300

    [BACK] refactor: tweaked endpoint of doublepoints

--- backend/app/Controllers/Payments/DoublePoints.ts
+++ backend/app/Controllers/Payments/DoublePoints.ts
@@ -1,7 +1,8 @@
 import Env from '@ioc:Adonis/Core/Env'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class DoublePointsController {
-  public async show() {
-    return Env.get('DOUBLE_POINTS');
+  public async show(ctx: HttpContextContract) {
+    return ctx.response.status(200).send({ status: 200, result: Env.get('DOUBLE_POINTS') });
   }
 }
--- backend/start/routes/payment.ts
+++ backend/start/routes/payment.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route';
 
-Route.get('/doublepoints', 'Payments/DoublePoints.show');
+Route.get('/payments/doublepoints', 'Payments/DoublePoints.show');
 Route.post('/payments/pix/qrcode', 'Payments/Pix.getQRCode');
 Route.post('/payments/pix/webhook(/pix)', 'Payments/Pix.webhook');
