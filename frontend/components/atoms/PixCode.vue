e70d8ed146a16f7189972eccecab986885c5e623
“WalistonBelles” <walistonbelles1@gmail.com>
2022-11-26 21:12:06 -0300

    [FRONT] feat: added qrcode and pix_code component

---
+++ frontend/components/atoms/PixCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-alert style="word-break: break-word;">
+    {{ code }}
+  </v-alert>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    code: {
+      type: String,
+      required: true
+    }
+  },
+})
+</script>
---
+++ frontend/components/atoms/QRCode.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="image" alt="QR Code">
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  },
+})
+</script>
